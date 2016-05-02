5554653c6cbb10fd6d278b5837bab3f894b664a8
PZoltan88 <pzoltan88@gmail.com>
2016-05-02 20:01:21 +0200

    Update ReceptKezelo.java

--- src/model/ReceptKezelo.java
+++ src/model/ReceptKezelo.java
@@ -77,9 +77,13 @@ public class ReceptKezelo extends Observable  implements AdatbazisKapcsolat{
             kapcsolatNyit();
             Statement s=kapcsolat.createStatement();
             
+            //System.out.println("User tábla létrehozása");
+            String sql_user_tabla =        
+            "CREATE TABLE User(nev varchar(255) PRIMARY KEY,password varchar(255))" ;
+            s.executeUpdate(sql_user_tabla);
             //System.out.println("Recept tábla létrehozása");
             String sql_recept_tabla =        
-                    "CREATE TABLE Recept(id int NOT NULL PRIMARY KEY,nev varchar(255),elkeszites varchar(2000))" ;
+                    "CREATE TABLE Recept(id int NOT NULL PRIMARY KEY,user_name varchar(255),nev varchar(255),elkeszites varchar(2000))" ;
             s.executeUpdate(sql_recept_tabla);
             //System.out.println("Recept sequence létrehozása");
             String sql_recept_sequence=
@@ -136,29 +140,29 @@ public class ReceptKezelo extends Observable  implements AdatbazisKapcsolat{
             kapcsolatNyit();
             Statement s=kapcsolat.createStatement();
             //System.out.println("Recept tesztadatok beszúrása Recept táblába");
-            String sql_recept_hozzad1 = "INSERT INTO Recept(id,nev,elkeszites) VALUES " +
-                    "(seq_recept.nextval,'Rántott csirkecomb kukoricás rizzsel','A csirkecombokat enyhén sós vízben megfőzzük, aztán bepanírozzuk először lisztbe, aztán tojásba, és végül a zsemlemorzsába forgatjuk bele.Bő olajban kisütjük a rántott csirkecombokat.Közben megfőzzük a rizst, kukoricát pirítunk hozzá, és összekeverjük a kukoricát a rizzsel.')";
+            String sql_recept_hozzad1 = "INSERT INTO Recept(id,user_name,nev,elkeszites) VALUES " +
+                    "(seq_recept.nextval,'default','Rántott csirkecomb kukoricás rizzsel','A csirkecombokat enyhén sós vízben megfőzzük, aztán bepanírozzuk először lisztbe, aztán tojásba, és végül a zsemlemorzsába forgatjuk bele.Bő olajban kisütjük a rántott csirkecombokat.Közben megfőzzük a rizst, kukoricát pirítunk hozzá, és összekeverjük a kukoricát a rizzsel.')";
             s.executeUpdate(sql_recept_hozzad1);
-            String sql_recept_hozzad2 = "INSERT INTO Recept(id,nev,elkeszites) VALUES " +
-                    "(seq_recept.nextval,'Holstein szelet hasábburgonyával','A húst klopfoljuk ki vékonyra, majd ízlés szerint sózzuk, borsozzuk, és hagyjuk kicsit állni, hogy az ízek jól átjárják." +
+            String sql_recept_hozzad2 = "INSERT INTO Recept(id,user_name,nev,elkeszites) VALUES " +
+                    "(seq_recept.nextval,'default','Holstein szelet hasábburgonyával','A húst klopfoljuk ki vékonyra, majd ízlés szerint sózzuk, borsozzuk, és hagyjuk kicsit állni, hogy az ízek jól átjárják." +
                     "Egy teflon serpenyőben hevítsük fel az olajat, majd az előzőleg a lisztbe megforgatott húst helyezzük bele, mérsékeljük a lángot, és sütés közben többször átfordítva, szép lassan süssük mindkét oldalát pirosra. Tálalásig tartsuk melegen." +
                     "A tojást szintén teflon serpenyőben, egy kevés olajon süssük meg, a sütési idő attól függ, hogy mennyire keményre szeretnénk sütni a sárgáját." +
                     "A krumplit pucoljuk meg, vágjuk hasábokra. Enyhén sós vízben főzzük kb. 5 percig, majd szűrjük le, hűtsük ki, ne maradjon vizes a krumpli! Tegyük be a fagyasztóba, hogy teljesen kifagyjon. (Érdemes egyszerre nagyobb mennyiséget előkészíteni, majd a fagyasztóban tárolni felhasználásig.)" +
                     "Öntsük bele az olajat egy serpenyőbe, hevítsük fel. A forró olajba óvatosan tegyünk bele kb. 2 nagy marék krumplit, keverjük kicsit össze, majd pár perc alatt süssük szép pirosra. (Sütés közben ne nagyon kevergessük, mert különben széttörik a krumpli.) Szűrőkanállal szedjük ki a megsült krumplit papírtörlőre, és tálalásig tartsuk melegen." +
                     "Tálalás előtt ízlés szerint sózzuk meg a krumplit, majd halmozzuk tányérra, helyezzünk a tetejére egy szelet sült húst és egy tükörtojást. Ízlés szerint szórjuk meg egy csipet pirospaprikával, majd úgy kínáljuk.')";
             s.executeUpdate(sql_recept_hozzad2);
-            String sql_recept_hozzad3 = "INSERT INTO Recept(id,nev,elkeszites) VALUES " +        
-                    "(seq_recept.nextval,'Rakott krumpli','A krumplikat 10-15 percre beáztatjuk hideg vízbe, majd alaposan megsikáljuk és leöblítjük. A tojásokat megmossuk." +
+            String sql_recept_hozzad3 = "INSERT INTO Recept(id,user_name,nev,elkeszites) VALUES " +        
+                    "(seq_recept.nextval,'default','Rakott krumpli','A krumplikat 10-15 percre beáztatjuk hideg vízbe, majd alaposan megsikáljuk és leöblítjük. A tojásokat megmossuk." +
                     "A krumplit annyi hideg vízzel öntjük fel, hogy bőven ellepje. Hozzáadunk 1 evőkanál sót (ettől ízletesebb lesz), és a forrástól számított 20 percig, fedő alatt, szelíd tűzön puhára főzzük. A tojásokat erősen sós, hideg vízben feltesszük főni, és a forrástól számított 8 percig főzzük, majd hideg vízbe merítve azonnal lehűtjük. A krumpli alól leöntjük a vizet, még melegen meghámozzuk, és hűlni hagyjuk. A kihűlt tojásokat ugyancsak meghámozzuk, elnegyedeljük vagy felkarikázzuk." +
                     "A kolbászt meleg vízzel leöblítjük (így könnyen lehúzhatjuk a héját), azután vékonyan felkarikázzuk. Elkeverjük a tejfölben a tojássárgákat. A sütőt 200 °C-ra (gázsütő 3. fokozat) előmelegítjük." +
                     "A margarinnal kikenünk egy lapos tűzálló tálat, aljára karikázzuk a krumpli felét, kissé megsózzuk, lazán összekeverjük, és elsimítjuk. Arányosan elosztva a tetejére rendezzük a tojást és a kolbászkarikákat, majd megkenjük a tejföl felével. A maradék krumplit felkarikázzuk, és kissé egymásra csúsztatva a tetejére rendezzük. Egyenletesen bevonjuk a maradék tejföllel, végül a tetejére göndörítjük a szalonnaszeleteket." +
                     "A sütőben addig sütjük, amíg a tejföl és a szalonna aranybarnára pirul (35-40 perc). Ezután tálalhatjuk.')";
             s.executeUpdate(sql_recept_hozzad3);
-            String sql_recept_hozzad4 = "INSERT INTO Recept(id,nev,elkeszites) VALUES " +
-                    "(seq_recept.nextval,'Mákos tészta','A darált mákot a porcukorral összekeverjük, majd a tésztát kifőzzük, leszűrjük. Ezután a vajat felmelegítjük egy lábosban. Majd beletesszük a tésztát, elkeverjük. Végül ozzháadjuk a porcukros mákot, majd összekeverjük.')";
+            String sql_recept_hozzad4 = "INSERT INTO Recept(id,user_name,nev,elkeszites) VALUES " +
+                    "(seq_recept.nextval,'default','Mákos tészta','A darált mákot a porcukorral összekeverjük, majd a tésztát kifőzzük, leszűrjük. Ezután a vajat felmelegítjük egy lábosban. Majd beletesszük a tésztát, elkeverjük. Végül ozzháadjuk a porcukros mákot, majd összekeverjük.')";
             s.executeUpdate(sql_recept_hozzad4);
-            String sql_recept_hozzad5 = "INSERT INTO Recept(id,nev,elkeszites) VALUES " +
-                    "(seq_recept.nextval,'Babgulyás','A babot előző este beáztatjuk jó sok vízbe. Másnap a répákat felkarikázzuk, a paradicsomot, és a húst is felaprítjuk. A babot egy edénybe tesszük, teszünk bele babérlevelet, felengedjük vízzel, ami ellepi és elkezdjük főzni had, puhuljon. A hagymát apróra vágjuk, kevés olajon üvegesre dinszteljük. Beletesszük az összenyomott fokhagymát, hozzáadjuk a megmosott felkockázott húst és fehéredésig főzzük. Hozzáadjuk, a pirospaprikát elkeverjük. Ezután mehet bele az apró kocára vágott paradicsom, a karikára vágott répa és zöldség. Felöntjük vízzel és hozzáadjuk a kisebb darabokra vágott füstölt húst. Fél óra főzés után a babot is beletesszük, és a bab főzőlevét is adjuk hozzá, nagyon finom lesz tőle. Együtt főzzük, amíg a hús és a bab is puha lesz. Addig elkészítjük a csipetkét. Egy tálba tesszük a lisztet közepébe a tojást, sót, és jól összegyúrjuk, ha kell, még adunk hozzá lisztet az a lényeg a tészta ne ragadjon a kezünkhöz. Ha puha a hús és a bab is, akkor a tésztát tegyük a levesbe, és jól forraljuk össze. A kész tészták feljönnek a leves tetejére.')" ;
+            String sql_recept_hozzad5 = "INSERT INTO Recept(id,user_name,nev,elkeszites) VALUES " +
+                    "(seq_recept.nextval,'default','Babgulyás','A babot előző este beáztatjuk jó sok vízbe. Másnap a répákat felkarikázzuk, a paradicsomot, és a húst is felaprítjuk. A babot egy edénybe tesszük, teszünk bele babérlevelet, felengedjük vízzel, ami ellepi és elkezdjük főzni had, puhuljon. A hagymát apróra vágjuk, kevés olajon üvegesre dinszteljük. Beletesszük az összenyomott fokhagymát, hozzáadjuk a megmosott felkockázott húst és fehéredésig főzzük. Hozzáadjuk, a pirospaprikát elkeverjük. Ezután mehet bele az apró kocára vágott paradicsom, a karikára vágott répa és zöldség. Felöntjük vízzel és hozzáadjuk a kisebb darabokra vágott füstölt húst. Fél óra főzés után a babot is beletesszük, és a bab főzőlevét is adjuk hozzá, nagyon finom lesz tőle. Együtt főzzük, amíg a hús és a bab is puha lesz. Addig elkészítjük a csipetkét. Egy tálba tesszük a lisztet közepébe a tojást, sót, és jól összegyúrjuk, ha kell, még adunk hozzá lisztet az a lényeg a tészta ne ragadjon a kezünkhöz. Ha puha a hús és a bab is, akkor a tésztát tegyük a levesbe, és jól forraljuk össze. A kész tészták feljönnek a leves tetejére.')" ;
             s.executeUpdate(sql_recept_hozzad5);
             
             //System.out.println("Recept tesztadatok beszúrása Összetevő táblába");
